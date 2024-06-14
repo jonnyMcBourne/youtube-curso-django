1f33435dd638ca9dd1164ae62128a3a7d6defa73
srOscuro <sroscuro@srOscuros-MacBook-Pro.local>
2024-06-14 12:48:44 -0600

    first commit configuration

---
+++ ecommerce/ecommerce/settings/local.py
@@ -0,0 +1,12 @@
+from .base import *
+DEBUG = True
+STATIC_URL = 'static/'
+ALLOWED_HOSTS = []
+# Database
+# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
